077f1ca30d22e109040d26ea235a154d8b6ae1b9
Prajwal Kundar <prajwalkundar8746@gmail.com>
2026-07-16 19:30:51 +0530

    solved the caching problem

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,6 @@
 import { redirect } from 'next/navigation';
 import { cookies } from 'next/headers';
+export const dynamic = 'force-dynamic';
 
 export default async function Home() {
   const cookieStore = await cookies();
